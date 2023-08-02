14a748fb501398050993b090c7345949a3111a26
Richy <richy@webcomm.com.tw>
2023-08-02 15:59:34 +0800

    Initial Commit

---
+++ iOS_RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_RandomUser
+//
+//  Created by 王麒翔 on 2023/8/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
